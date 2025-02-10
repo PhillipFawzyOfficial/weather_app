d2ee4c02ae75d0f4313521ce52ceac49fefbde73
Phillip Fawzy <flipking037@gmail.com>
2025-02-10 04:17:03 +0200

    Use Try catch To Catch Exception

--- lib/services/weather_service.dart
+++ lib/services/weather_service.dart
@@ -7,12 +7,21 @@ class WeatherService {
   final String baseUrl = 'http://api.weatherapi.com/v1';
   final String apiKey = '15736d145ed14d54a6f210947241201';
 
-  Future<WeatherModel> getWeather() async {
-    Response response =
-        await dio.get('$baseUrl/forecast.json?key=$apiKey&q=Cairo');
+  Future<WeatherModel?> getWeather() async {
+    try {
+      Response response =
+          await dio.get('$baseUrl/forecast.json?key=$apiKey&q=Cairo');
 
-    WeatherModel weatherModel = WeatherModel.fromJson(response.data);
+      WeatherModel weatherModel = WeatherModel.fromJson(response.data);
 
-    return weatherModel;
+      return weatherModel;
+    } on DioException catch (e) {
+      final String errorMesage = e.response?.data['error']['message'] ??
+          'Opes There Was An Error, try Later';
+
+      throw Exception(errorMesage);
+    } catch (e) {
+      throw Exception('Opes There Was An Error, try Later');
+    }
   }
 }
