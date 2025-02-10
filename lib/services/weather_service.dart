1ca764708a34c4dc96177a71c5bc53ad05f4aa76
Phillip Fawzy <flipking037@gmail.com>
2025-02-10 03:14:54 +0200

    Create Variable baseUrl & apiKey

--- lib/services/weather_service.dart
+++ lib/services/weather_service.dart
@@ -4,9 +4,12 @@ import 'package:weather/models/weather_model.dart';
 class WeatherService {
   final Dio dio = Dio();
 
+  final String baseUrl = 'http://api.weatherapi.com/v1';
+  final String apiKey = '15736d145ed14d54a6f210947241201';
+
   Future<WeatherModel> getWeather() async {
-    Response response = await dio.get(
-        'http://api.weatherapi.com/v1/forecast.json?key=15736d145ed14d54a6f210947241201&q=Cairo');
+    Response response =
+        await dio.get('$baseUrl/forecast.json?key=$apiKey&q=Cairo');
 
     WeatherModel weatherModel = WeatherModel.fromJson(response.data);
 
