0c275deb494615a7685d18b1e5b871ac24add277
Phillip Fawzy <flipking037@gmail.com>
2025-02-10 02:55:43 +0200

    Use factory in WeatherModel

--- lib/models/weather_model.dart
+++ lib/models/weather_model.dart
@@ -2,9 +2,9 @@ class WeatherModel {
   final String cityName;
   final String time;
   final String? image;
-  final String temp;
-  final String maxTemp;
-  final String minTemp;
+  final double temp;
+  final double maxTemp;
+  final double minTemp;
   final String weatherConsition;
 
   WeatherModel(
@@ -15,4 +15,17 @@ class WeatherModel {
       required this.maxTemp,
       required this.minTemp,
       required this.weatherConsition});
+
+  factory WeatherModel.fromJson(json) {
+    return WeatherModel(
+      cityName: json['location']['name'],
+      time: json['current']['last_updated'],
+      image: json['forecast']['forecastday'][0]['day']['condition']['icon'],
+      temp: json['forecast']['forecastday'][0]['day']['avgtemp_c'],
+      maxTemp: json['forecast']['forecastday'][0]['day']['maxtemp_c'],
+      minTemp: json['forecast']['forecastday'][0]['day']['mintemp_c'],
+      weatherConsition: json['forecast']['forecastday'][0]['day']['condition']
+          ['text'],
+    );
+  }
 }
