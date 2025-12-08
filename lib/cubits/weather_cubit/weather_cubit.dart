2a4e5315e615451ccd0dada8629a2cfd1860a357
phillip <flipking037@gmail.com>
2025-12-08 18:34:04 +0200

    Finsh App

--- lib/cubits/weather_cubit/weather_cubit.dart
+++
@@ -1,6 +0,0 @@
-import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:weather/cubits/weather_cubit/weather_state.dart';
-
-class WeatherCubit extends Cubit<WeatherState> {
-  WeatherCubit() : super(WeatherLoading());
-}
